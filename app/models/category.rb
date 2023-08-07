50140b0f25ba3e428b8ef8415cbcbb73b710a8fe
navpannu32 <pannunav74@gmail.com>
2023-08-08 01:35:42 +0500

    category model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
     has_many :products
 
+    validates :name, presence: true
+    validates :description, length: {maximum: 500}
+
     def self.ransackable_attributes(auth_object = nil)
         ["created_at", "description", "id", "name", "updated_at"]
     end
