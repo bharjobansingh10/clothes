2615eaee49b6f16930d051dc181d3dbda42824ac
navpannu32 <pannunav74@gmail.com>
2023-08-08 01:35:59 +0500

    active admin validation

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -3,4 +3,6 @@ ActiveAdmin.register Product do
   actions :index, :edit, :update, :create, :destroy, :new
   permit_params :name, :price, :description, :category_id, :image, :is_featured
 
+  Formtastic::FormBuilder.perform_browser_validations = true
+
 end
