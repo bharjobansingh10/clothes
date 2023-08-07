af2c0159fd60cfc78b7f94f75644a9cd5f434286
navpannu32 <pannunav74@gmail.com>
2023-08-08 01:36:09 +0500

    active admin validations

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -2,5 +2,7 @@ ActiveAdmin.register Category do
 
   actions :index, :edit, :update, :create, :destroy, :new
   permit_params :name, :description
-  
+
+  Formtastic::FormBuilder.perform_browser_validations = true
+
 end
