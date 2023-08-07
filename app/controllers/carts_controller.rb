e91edf11859ed7335d536e2e320dc6fa275117e6
navpannu32 <pannunav74@gmail.com>
2023-08-08 01:26:56 +0500

    cart must be 1 product

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -18,9 +18,9 @@ class CartsController < ApplicationController
     product_id = params[:product_id].to_s
 
     item = cart[product_id] || { "quantity" => 1 }
-    item["quantity"] -= 1
+    item["quantity"] -= 1 if cart.count > 1
     cart[product_id] = item
-    cart.delete(product_id) if item["quantity"] < 1
+    cart.delete(product_id) if item["quantity"] < 1 && cart.count > 1
     update_cart cart
 
     redirect_to cart_path
